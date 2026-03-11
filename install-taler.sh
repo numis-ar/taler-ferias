8dab89d07973581b11a602eaade3829587d8db84
David Coronel <davidcoronel@gmail.com>
2026-03-11 02:29:48 -0300

    Fix nginx config: add missing closing brace for /config location

--- install-taler.sh
+++ install-taler.sh
@@ -385,6 +385,7 @@ server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_set_header Authorization \$http_authorization;
+    }
 
     # Merchant /config for payments (returns taler-merchant)
     location /merchant/config {
@@ -394,8 +395,6 @@ server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_set_header Authorization \$http_authorization;
-    }
-        
     }
 
     # Redirect /exchange to /exchange/
